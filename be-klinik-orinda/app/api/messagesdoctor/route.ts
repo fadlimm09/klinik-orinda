00ed74d2bdfb6272c9b58b4b6a847d91ab0ef4f8
Muhamad Alfito Santosa <sanskun02pg@gmail.com>
2025-04-29 20:59:44 +0700

    pus fix lint

--- be-klinik-orinda/app/api/messagesdoctor/route.ts
+++ be-klinik-orinda/app/api/messagesdoctor/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: Request) {
   try {
     const { senderId } = await req.json();
     const message = await prisma.message.findMany({
