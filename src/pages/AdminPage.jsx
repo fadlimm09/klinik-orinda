0405176d01a95c64f1351529398f594ddef3106b
Muhamad Alfito Santosa <sanskun02pg@gmail.com>
2025-05-14 10:48:46 +0700

    push chat for admin page

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -18,14 +18,19 @@ function AdminPage() {
       <div>
         <h2>Admin Page</h2>
         <p>Halaman ini adalah halaman admin. Disini admin bisa melakukan penghapusan, dan perubahan data.</p>
+        <div className="d-flex gap-3 ">
+          <Button href="https://klinik-orinda-chat-layering.vercel.app/chatdoctor/dokterAgah">Chat Dokter Agah </Button>
+          <Button href="https://klinik-orinda-chat-layering.vercel.app/chatdoctor/dokterDesy">Chat Dokter Desy</Button>
+          <Button href="https://klinik-orinda-chat-layering.vercel.app/chatdoctor/dokterOno">Chat Dokter Ono</Button>
+        </div>
         <div className="d-flex gap-3">
           <Button onClick={() => handleButtonClick("DataBooking")}>Data Booking</Button>
 
           <Button onClick={() => handleButtonClick("KategoriDokter")}>Data Kategori</Button>
 
           <Button onClick={() => handleButtonClick("DataPasien")}>Data Pasien</Button>
-          <Button href="https://klinik-orinda-chat-layering.vercel.app/chatdoctor">Chat Admin</Button>
         </div>
+
         <div>
           {selectedComponent === "DataBooking" && <DataBookingAdmin />}
           {selectedComponent === "KategoriDokter" && <KategoriDokterAdmin />}
